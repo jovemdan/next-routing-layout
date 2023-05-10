d98216e6668b56f0170d7f6dd3e1afba1308f6c9
jovemdan <danilogdev@gmail.com>
2023-05-10 09:54:40 -0300

    feat: Create component nav-link

---
+++ app/nav-link.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function NavLink({ href, children }: any) {
+  const pathname = usePathname()
+  console.log(pathname)
+  return <Link href={href}>{children}</Link>
+}
